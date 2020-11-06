75fc121c7c4c00226bc77c06a3b52d26c5bbe14a
Alp <49457057+alpkeskin@users.noreply.github.com>
2020-11-06 15:33:51 +0300

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,20 @@
+# https://github.com/alpkeskin
+try:
+	import pyautogui
+	import time
+except:
+	print("Error! You have to download the required module. Write this to your terminal : pip install PyAutoGUI ")
+print("Click where you want to write.")
+try:
+	time.sleep(5)
+	f = open("commands.txt", "r")
+	for word in f:
+		pyautogui.typewrite(word)
+		time.sleep(2)
+		if word == "Q":
+			print("Done.")
+			pyautogui.press("enter")
+			f.close()
+			exit()
+except KeyboardInterrupt:
+	print("Exit.")
